93a597ea7b9f83e77d3eebd64a89b5a5f062167f
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-08-07 13:17:38 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,397 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-204219461-1">
+    </script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-204219461-1');
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	<link rel="icon" type="imagem/png" href="img/logo2.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+	<title>D'Roma Duplex De Luxo</title>
+</head>
+<body>
+
+		
+
+
+		<nav class=" navbar navbar-expand-lg navbar-dark  bg-dark" id="topo" style="width: 100%;">
+			<div class="container-fluid">
+				<a class="navbar-brand">
+					<img src="img/logo2.png" alt="" width="60" height="52">
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				      <span class="navbar-toggler-icon"></span>
+				    </button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				    	<li class="nav-item">
+				    		<a class="nav-link active" id="linknav" aria-current="page" href="#parallax">Inicio	</a>
+				    	</li>
+				    	<li class="nav-item">
+				    		<a class="nav-link" id="linknav" href="#parte2">Video</a>
+				    	</li>
+				    	<li class="nav-item">
+				        	<a class="nav-link" id="linknav" href="#parte3" tabindex="-1" aria-disabled="true">	informações</a>
+				    	</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<div class="parallax" id="parallax" style="height:150px; background-color:white;font-size:36px">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<h1 style="margin-top: 80px;">Gonçalo Monteiro</h1>
+					</div>
+					<div class="col-sm-6" id="contato" style="">
+						<form method="post" action="envio.php">
+							<center><h4 id="title">Tem interesse? Entre em contato conosco!</h4></center>
+							<input type="text" name="nome" placeholder="Nome" class="form-control" id="inpt">
+							<input type="email" name="email" placeholder="Email" class="form-control" id="inpt">
+							<input type="number" name="celular" max="11" placeholder="Celular" class="form-control" id="inpt">
+							<input type="number" name="telefone" max="10" placeholder="Telefone" class="form-control" id="inpt">
+
+							<center><input type="submit" value="Entrar em Contato" class="btn btn-outline-danger" id="inptf"></center>
+						</form>
+					</div>	
+				</div>
+			</div>
+			<div class="container">
+			    <div class="row">
+					<div class="col-sm-12">
+						<center><h1 id="titulovalor" >UNIDADES A PARTIR DE R$195.000,00</h1></center>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- <div class="cond">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<center><h2 id="titulovideo">Uma Oportunidade Única!</h2></center>
+						<p></p>
+					</div>
+				</div>
+			</div>
+		</div> -->		
+<hr style="opacity : 0">
+		<div id="parte2" class="" style="height:100%; background-color: white; ">
+			<div class="container ">
+				<div class="row">
+					<div class="col-sm-12">
+						<center><h2 id="titulovideo titulos">ACOMPANHE UM POUCO DA OBRA</h2></center>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">
+						<center><iframe  height="380" style="width: 60%;" 
+						src="https://www.youtube.com/embed/mQ1SvzuJWJw?autoplay=0&mute=1" style="border-color: black; margin-bottom: 10px;">
+						</iframe></center>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Opção imagens <div class="imgs" style="height: 100%;">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<img src="img/imovel/olx0.jpeg" class="img-fluid">
+					</div>
+					<div class="col-sm-6">
+						<div class="row">
+							<div class="col-sm-6">
+								<img src="img/imovel/olx1.jpeg" class="img-fluid">
+							</div>
+							<div class="col-sm-6">
+								<img src="img/imovel/olx1.jpeg" class="img-fluid">
+							</div>
+						</div>
+						<div class="row" style="margin-top: 15px;">
+							<div class="col-sm-6">
+								<img src="img/imovel/olx1.jpeg" class="img-fluid">
+							</div>
+							<div class="col-sm-6">
+								<img src="img/imovel/olx1.jpeg" class="img-fluid">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> -->
+
+		<hr>
+
+		<div id="parte3">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<center><h2 id="titulos" style="font-weight: bold;">SOBRADOS EM CONDOMÍNIO FECHADO</h2></center>
+					</div>
+					<div class="col-sm-12" id="sub" style="margin-bottom: 25px;">
+						<center><h3 id="titulos">COM ACABAMENTO DE ALTO PADRÃO</h3></center>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6" id="car">
+						<div class="carrosel">
+							<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+								<ol class="carousel-indicators">
+									<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="11"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="12"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="13"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="14"></li>
+								</ol>
+								<div class="carousel-inner">
+									<div class="carousel-item active"><!-- 1-->
+										<img src="img/imovel/fachada1.jpeg" class="d-block w-100 img-fluid" alt="Primeiro Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Entrada</h5>
+											<p>Conheça a Entrada do Condomínio</p>
+										</div>
+									</div>
+									<div class="carousel-item"><!-- 1-->
+										<img src="img/imovel/aa.jpg" class="d-block w-100 img-fluid" alt="Primeiro Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Entrada</h5>
+											<p>Conheça a Entrada do Condomínio</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/olx1.jpeg" class="d-block w-100" alt="Segundo Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Corredor Principal</h5>
+											<p>Area Comum de Acesso às Casas</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/olx2.jpeg" class="d-block w-100" alt="Terceiro Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Garagem</h5>
+											<p>Espaço para Guardar seu veículo</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/sala1.jpeg" class="d-block w-100" alt="Quarto Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Entrada Sala</h5>
+											<p>A Primeira Vista do seu futuro lar</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/olx4.jpeg" class="d-block w-100" alt="Quinto Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Sala de Jantar</h5>
+											<p>Espaço Perfeito Para um Almoço em Família</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/olx5.jpeg" class="d-block w-100" alt="Sexto Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Sala de Jantar + Area</h5>
+											<p>Espaço Perfeito Para um Almoço em  Família</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/olx6.jpeg" class="d-block w-100" alt="Setimo Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Sala de Jantar + Cozinha</h5>
+											<p>Conheça a cozinha independente</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/olx7.jpeg" class="d-block w-100" alt="Oitavo Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Geral Sala</h5>
+											<p>Um Outro Ângulo para a Entrada</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/area1.jpeg" class="d-block w-100" alt="Nono Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Espaço Gourmet</h5>
+											<p>Local ideal para Confraternização e Lazer</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/quarto1.jpeg" class="d-block w-100 img-fluid" alt="Decimo Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Quarto</h5>
+											<p>Conheça a suíte.</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/quarto2.jpeg" class="d-block w-100 img-fluid" alt="Decimo prim Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Quarto</h5>
+											<p>Conheça a suíte.</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/olx11.jpeg" class="d-block w-100 img-fluid" alt="Decimo seg Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Espaço Home Office</h5>
+											<p>Ideal para Trabalhar e Estudar em Casa</p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/olx12.jpeg" class="d-block w-100 img-fluid" alt="Decimo ter Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Banheiro</h5>
+											<p></p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/olx13.jpeg" class="d-block w-100 img-fluid" alt="Decimo quar Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Banheiro</h5>
+											<p></p>
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="img/imovel/olx14.jpeg" class="d-block w-100 img-fluid" alt="Decimo quint Slide">
+										<div class="carousel-caption d-none d-md-block">
+											<h5>Quarto + banheiro</h5>
+											<p>Uma nova perspectiva para a suíte</p>
+										</div>
+									</div>
+								</div>
+								<a href="#carouselExampleIndicators" role="button" data-slide="prev" class="carousel-control-prev">
+									<span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Anterior</span>
+								</a>
+								<a href="#carouselExampleIndicators" role="button" data-slide="next" class="carousel-control-next">
+									<span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Próximo</span>
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6" id="infos">
+						<div class="row" style="margin-top: 10px;">
+							<div class="col-sm-4">
+								<center><img src="img/icone/valor.png" style="height: 40px; width: 40px;">
+								<b><p>R$ 195.000,00</p></b></center>
+							</div>
+							<div class="col-sm-4">
+								<center><img src="img/icone/garagem.png" style="height: 40px; width: 40px;">
+								<b><p>1 Vaga</p></b></center>
+							</div>
+							<div class="col-sm-4">
+								<center><img src="img/icone/cama.png" style="height: 40px; width: 40px;">
+								<b><p>2 Quartos</p></b></center>
+							</div>
+						</div>
+						<div class="row" style="margin-top: 15px;">
+							<div class="col-sm-4">
+								<center><img src="img/icone/piscina.png" style="height: 40px; width: 40px;">
+								<b><p>Piscina Priv.</p></b></center>
+							</div>
+							<div class="col-sm-4">
+								<center><img src="img/icone/churrasqueira.png" style="height: 40px; width: 40px;">
+								<b><p>Churrasqueira Priv.</p></b></center>
+							</div>
+							<div class="col-sm-4">
+								<center><img src="img/icone/chuveiro.png" style="height: 40px; width: 40px;">
+								<b><p>1 Banheiro</p></b></center>
+							</div>
+						</div>
+						<div class="row" style="margin-top: 10px;">
+							<div class="col-sm-4">
+								<center><img src="img/icone/quiosque.png" style="height: 40px; width: 40px;">
+								<b><p>Portaria 24H</p></b></center>
+							</div>
+							<div class="col-sm-4">
+								<center><img src="img/icone/plano.png" style="height: 40px; width: 40px;">
+								<b><p>55 m²</p></b></center>
+							</div>
+							<div class="col-sm-4">
+								<center><img src="img/icone/finan.png" style="height: 40px; width: 40px;">
+								<b><p>Financiamento com a Construtora</p></b></center>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row" id="mapa">
+					<div class="col-sm-6" >
+						<div class="row">
+							<div class="col-sm-12">
+								<center><h1 id="ttexto titulos">Quer saber mais?</h1></center>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-12">
+								<p style="text-align: justify;"><b>14 UNIDADES DE DUPLEX DE LUXO EM CONDOMÍNIO FECHADO COM ACABAMENTO DE ALTO PADRÃO.</b> Sendo: sala dois ambientes, lavabo, duas suítes, espaço home office, ambiente gourmet, piso porcelanato, churrasqueira, portão automático, uma vaga de garagem, interfone e alarme.
+								Em <b>fase de acabamento</b>, com data de entrega para <b>1° de Dezembro de 2021</b>.
+								Entrada de <b>R$ 37.000,00</b> e parcelas de <b>R$ 2.000,00</b> durante a obra. ( Aceitamos veículos como parte de pagamento).</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<center><h1 id="ttexto titulos">Localização do Imóvel</h1></center>
+						<center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d909.8033583388842!2d-46.83749961608494!3d-24.19930342825936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d02aa51e50c389%3A0x6b0908dbec0e19ae!2sAv.%20Gon%C3%A7alo%20Monteiro%2C%201050%20-%20Cibratel%20II%2C%20Itanha%C3%A9m%20-%20SP%2C%2011740-000!5e0!3m2!1spt-BR!2sbr!4v1628351078086!5m2!1spt-BR!2sbr" style="border:0; width: 80%; height: 100%;" allowfullscreen="" loading="lazy"></iframe></center>
+					</div>
+				</div>
+				<div class="row" id="mapa">
+					<div class="col-sm-6">
+						<center><h1 id="ttexto titulos" >Quem Somos</h1></center>
+						<p style="text-align: justify;">Com mais de 14 anos de uma trajetória marcada pelas realizações inovadoras, conquistamos nossa credibilidade e a sua confiança construindo residências e condomínios que inspiram segurança, conforto e comodidade. Nosso objetivo é a construção de novos projetos de vida aliados à tecnologia, design, sofisticação e um excelente acabamento, valorizando as parcerias e pessoas envolvidas em todos os processos. Tudo isso nos torna sinônimo de empreendedorismo, experiência e profissionalismo no mercado imobiliário local e regional. Temos orgulho de proporcionar aos nossos clientes investidores a satisfação de adquirirem imóveis na planta, com a certeza e segurança de que o imóvel adquirido será entregue. Por isso, estamos sempre antenados às novas tecnologias, tendências e exigências do mercado construtivo, de modo a garantir mais solidez para a sua obra e maior satisfação para os nossos clientes.</p>
+					</div>
+					<div class="col-sm-6">
+						<center><h1 id="ttexto titulos">Nossa Localização</h1></center>
+						<center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d909.8520560477822!2d-46.83215047355779!3d-24.192478391084805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d02bcbdc17a867%3A0x331f524a103273ea!2sMS%20Soares%20Construtora%20e%20Incorporadora!5e0!3m2!1spt-BR!2sbr!4v1628350197673!5m2!1spt-BR!2sbr" style="border:0; width: 80%; height: 100%;" allowfullscreen="" loading="lazy"></iframe></center>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<footer class="page-footer" style="margin-top: 20px;">
+
+			<a href="https://wa.me/5513981194675?text=Tenho%20Interesse%20no%20Duplex" style="position:fixed;width:60px;height:60px;bottom:40px;right:40px;background-color:#25d366;color:#FFF;border-radius:50px;text-align:center;font-size:30px;box-shadow: 1px 1px 2px #888;
+			  	z-index:1000;" target="_blank">
+				<i style="margin-top:16px" class="fa fa-whatsapp"></i>
+				<p style="font-size: 15px; margin-top: 7px; color: lightgreen;">Fale Conosco</p>
+			</a>
+
+			<p>
+				<p>D'Roma Duplex de Luxo, um projeto <a href="http://mssoares.com.br/">MS Soares Construtora e Incorporadora </a><br> Site desenvolvido por <a href="https://github.com/CalixtoM">Matheus Calixto</a>.</p>
+			<a href="#topo">Voltar ao Topo</a>
+				</p>
+
+		</footer>
+	
+
+</body>
+</html>
+
+<style type="text/css">
+
+</style>
